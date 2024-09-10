5b0f511a03e1724dafb9f6b07f862b695db22c46
dperformance <docyoungs@naver.com>
2024-09-10 14:14:14 +0900

    feat: [hello_tobyspring] - 관계설정 책임의 분리 PaymentService 앞단에 Client가 관계설정 책임을 갖는다

--- java/hello_tobyspring/src/main/java/com/dev/PaymentService.java
+++ java/hello_tobyspring/src/main/java/com/dev/PaymentService.java
@@ -8,9 +8,8 @@ public class PaymentService {
 
     private final ExRateProvider exRateProvider;
 
-    public PaymentService() {
-//        this.exRateProvider = new WebApiExRateProvider();
-        this.exRateProvider = new SimpleExRateProvider();
+    public PaymentService(ExRateProvider exRateProvider) {
+        this.exRateProvider = exRateProvider;
     }
 
     public Payment prepare(Long orderId, String currency, BigDecimal foreignCurrencyAmount) throws IOException {
