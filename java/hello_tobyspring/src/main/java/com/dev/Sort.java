520311c6d304716d246a9556deffa4e64071f8e3
dperformance <docyoungs@naver.com>
2024-10-21 14:21:57 +0900

    test: [hello_tobyspring] - sort 기능 테스트

--- java/hello_tobyspring/src/main/java/com/dev/Sort.java
+++ java/hello_tobyspring/src/main/java/com/dev/Sort.java
@@ -1,16 +1,11 @@
 package com.dev;
 
-import java.util.Arrays;
 import java.util.List;
 
 public class Sort {
-    public static void main(String[] args) {
-//        List<Integer> scores = Arrays.asList(5, 7, 1, 9, 2, 8);
-//        List<String> scores = Arrays.asList("a", "z", 'f', "k", "i", "j");
-        List<String> scores = Arrays.asList("z", "x", "Spring", "java");
-        scores.sort((a1, a2) -> a1.length() - a2.length());
 
-        scores.forEach(System.out::println);
-//        Collections.sort();
+    public List<String> sortByLength(List<String> list) {
+        list.sort((a1, a2) -> a1.length() - a2.length());
+        return list;
     }
 }
